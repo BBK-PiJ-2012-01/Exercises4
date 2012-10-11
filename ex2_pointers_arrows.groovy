25b8ae98ed941b16c829ddd728300b1f27335cda
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2012-10-11 16:58:58 +0100

    started ex2a

--- ex2_pointers_arrows.groovy
+++ ex2_pointers_arrows.groovy
@@ -1,19 +1,15 @@
+// Stack is where simple types and complex pointer types are stored statically.
+// Heap is where complex (boxed) types are stored dynamically.
+
 class Point { 
     int x;
     int y; 
 }
-// This method increments the int by 1 and // moves the point to the right
+
 void increment(Point point, int n) {
-    n = n + 1;                // Local copy of n is incremented.  Not myInt!
-    
-    point.x = point.x + 1;    // point is boxed, so passed by reference to increment
-                              // meaning this increments myPoint.x
-    
-    point = null;             // point is just a reference to myPoint.  Setting
-                              // this to null doesn't affect myPoint.
-                              // NB. if all references to myPoint are removed,
-                              // the garbage collector might delete the
-                              // myPoint boxed object.
+    n = n + 1;                
+    point.x = point.x + 1;    
+    point = null;             
     println " At the end of the method...";
     println " The integer is " + n; 
     println " The point is " + point;
@@ -41,3 +37,8 @@ At the end of the method... The integer is 1
 The point is null
 The integer is now 0 The point is now 1,0
 */
+
+
+/*    Stack:
+  - Point class definition
+  - 
